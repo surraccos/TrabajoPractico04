096bb973b3a630700dbd8fe61a30ea43dd3c191f
surraccos <105308208+surraccos@users.noreply.github.com>
2022-11-18 18:34:45 -0300

    Creación de estructura del repositorio de usuarios.

---
+++ TrabajoPractico04/IRepositorioUsuarios.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio03
+{
+    internal class IRepositorioUsuarios
+    {
+      
+        //Metodos
+        internal void Agregar(Usuario pUsuario)
+        {
+
+            array[count] = pBanca;
+            count++;
+
+        }
+
+        internal void Actualizar(Usuario pUsuario)
+        {
+
+            array[count] = pBanca;
+            count++;
+
+        }
+
+        internal void Eliminar(string pCodigo)
+        {
+
+            array[count] = pBanca;
+            count++;
+
+        }
+
+        internal IList<Usuario> ObtenerTodos() 
+        {
+            return ;
+        }
+        
+        internal Usuario ObtenerPorCodigo(string pCodigo)
+        {
+            return;
+        }
+
+
+        //Implementar 3 de estos
+        internal IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparador)
+        {
+            return;
+        }
+
+    }
+}
---
+++ TrabajoPractico04/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio03
+{
+    public class Usuario
+    {
+        //Atributos de la clase
+        public string iCodigo;
+        public string iNombreCompleto;
+        public string iCorreoElectronico;
+
+        //Propiedades X e Y
+        public string Codigo
+        {
+            get { return this.iCodigo; }
+            set { this.iCodigo = value; }
+        }
+
+        public string NombreCompleto
+        {
+            get { return this.iNombreCompleto; }
+            set { this.iNombreCompleto = value; }
+        }
+
+        public string CorreoElectronico
+        {
+            get { return this.iCorreoElectronico; }
+            set { this.iCorreoElectronico = value; }
+        }
+
+        //Métodos 
+        
+    }
+}
